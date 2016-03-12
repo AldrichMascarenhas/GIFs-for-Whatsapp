0a1979beb19a91245c86b1fb426e5c7b9d749099
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2016-03-13 04:21:18 +0530

    Add Button behaviors and Intent Extras

--- app/src/main/java/com/nerdcutlet/gift/activities/MainActivity.java
+++ app/src/main/java/com/nerdcutlet/gift/activities/MainActivity.java
@@ -1,30 +1,75 @@
 package com.nerdcutlet.gift.activities;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.nerdcutlet.gift.BuildConfig;
 import com.nerdcutlet.gift.R;
+import com.nerdcutlet.gift.models.giphy.Datum;
+import com.nerdcutlet.gift.models.giphy.GIFModelMain;
+import com.nerdcutlet.gift.network.GiphyApi;
+import com.nerdcutlet.gift.network.GiphyApiInterface;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "MainActivity";
+    boolean ifGif = true;
+
+
     @Bind(R.id.gif_selected_button)
     Button gifSelectedButton;
+
+    @OnClick(R.id.gif_selected_button)
+    public void gifSelected() {
+        ifGif = true;
+        gifSelectedButton.setBackgroundColor(ContextCompat.getColor(this, R.color.selected));
+        stickerSelectedButton.setBackgroundColor(ContextCompat.getColor(this, R.color.notselected));
+    }
+
+
     @Bind(R.id.sticker_selected_button)
-    Button  stickerSelectedButton;
-    @Bind(R.id.search_textview_field)
-    AutoCompleteTextView searchAutoComplete;
+    Button stickerSelectedButton;
+
+    @OnClick(R.id.sticker_selected_button)
+    public void stickerSelected() {
+        ifGif = false;
+        gifSelectedButton.setBackgroundColor(ContextCompat.getColor(this, R.color.notselected));
+        stickerSelectedButton.setBackgroundColor(ContextCompat.getColor(this, R.color.selected));
+    }
+
+
+    @Bind(R.id.search_edittext_field)
+    EditText searchEditText;
+
+
     @Bind(R.id.search_button)
     Button searchButton;
     @Bind(R.id.search_voice_btn)
@@ -41,15 +86,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    sendMessage(searchEditText.getText().toString(), ifGif);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), GifDisplayActivity.class);
+                startActivity(i);
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
             }
         });
+    }
+
+    public void sendMessage(String searchParams, boolean ifGif) {
+        Intent i = new Intent(getApplicationContext(), GifDisplayActivity.class);
+        i.putExtra("ifGif", ifGif);
+        i.putExtra("search_param", searchParams);
+        startActivity(i);
+
     }
 
     @Override
