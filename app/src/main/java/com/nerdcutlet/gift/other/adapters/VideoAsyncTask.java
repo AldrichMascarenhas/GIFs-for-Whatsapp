69bb539f875d4e568e627abb36980b85cb5518a7
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2016-03-29 02:21:51 +0530

    Video downlaod and Play in Activty

--- app/src/main/java/com/nerdcutlet/gift/other/adapters/VideoAsyncTask.java
+++ app/src/main/java/com/nerdcutlet/gift/other/adapters/VideoAsyncTask.java
@@ -7,10 +7,25 @@ import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.nerdcutlet.gift.R;
 import com.nerdcutlet.gift.utils.Utils;
+import com.nerdcutlet.gift.utils.VideoDownloadResponse;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by Aldrich on 27-03-2016.
@@ -19,6 +34,7 @@ public class VideoAsyncTask extends AsyncTask<Void, Void, Void> {
 
     public final static String LOG_TAG = "VideoAsyncTask";
 
+    public VideoDownloadResponse videoDownloadResponse = null;
     String VideoUrl;
     Context context;
 
@@ -27,11 +43,9 @@ public class VideoAsyncTask extends AsyncTask<Void, Void, Void> {
 
     File myExternalFile;
 
-    String filepath = "MyFileStorage";
-    String filename = "myfile";
+
+    String filename = "myfile.mp4";
     String string = "Hello world!";
-
-
 
 
     public void setData(Context context, String url) {
@@ -42,30 +56,60 @@ public class VideoAsyncTask extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
 
-
-
-
-        //check if external storage is available and not read only
-        if (!utils.isExternalStorageAvailable() || utils.isExternalStorageReadOnly()) {
-            Log.d(LOG_TAG, "No Ext");
-        } else {
-            myExternalFile = new File(context.getExternalFilesDir(filepath), filename);
-        }
+        OkHttpClient client = new OkHttpClient();
         try {
+            Request request = new Request.Builder()
+                    .url(VideoUrl)
+                    .build();
+            Response response = client.newCall(request).execute();
+            Log.d(LOG_TAG, "Response : " + response.toString());
+            Log.d(LOG_TAG, "Response : " + response.body().toString());
 
-            FileOutputStream fos = new FileOutputStream(myExternalFile);
-            fos.write(string.getBytes());
-            fos.close();
-            Log.d(LOG_TAG, "Done!");
+            //check if external storage is available and not read only
+            if (!utils.isExternalStorageAvailable() || utils.isExternalStorageReadOnly()) {
+                Log.d(LOG_TAG, "No Ext");
+            } else {
+                myExternalFile = new File(context.getExternalFilesDir(context.getString(R.string.storage_filepath)), filename);
+            }
+            try {
+
+                InputStream is = response.body().byteStream();
+                BufferedInputStream input = new BufferedInputStream(is);
+                OutputStream output = new FileOutputStream(myExternalFile);
+
+                byte[] data = new byte[1024];
+
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+                output.close();
+                input.close();
+
+
+                Log.d(LOG_TAG, "Done!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
         } catch (Exception e) {
-            e.printStackTrace();
+
         }
+
+
         return null;
     }
 
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
+        videoDownloadResponse.localVideoUrl(myExternalFile.getAbsolutePath());
+
     }
 
     @Override
---
+++ app/src/main/java/com/nerdcutlet/gift/utils/VideoDownloadResponse.java
@@ -0,0 +1,8 @@
+package com.nerdcutlet.gift.utils;
+
+/**
+ * Created by Aldrich on 28-03-2016.
+ */
+public interface VideoDownloadResponse {
+    void localVideoUrl(String videoUrl);
+}
