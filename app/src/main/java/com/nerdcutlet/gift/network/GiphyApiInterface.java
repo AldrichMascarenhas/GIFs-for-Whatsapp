7be4dbc2c5b4888784f5535a5b68e7860ecdd375
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2016-03-30 21:25:05 +0530

    Add new getGifsByID Call

--- app/src/main/java/com/nerdcutlet/gift/network/GiphyApiInterface.java
+++ app/src/main/java/com/nerdcutlet/gift/network/GiphyApiInterface.java
@@ -3,9 +3,14 @@ package com.nerdcutlet.gift.network;
 import com.nerdcutlet.gift.models.giphy.GIFModelMain;
 import com.nerdcutlet.gift.models.giphy.random.RandomGIFModel;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * Created by Aldrich on 08-03-2016.
@@ -28,6 +33,10 @@ public interface GiphyApiInterface {
 
     @GET("gifs/random")
     Call<RandomGIFModel> getRandomGif(@Query("tag") String tag, @Query("rating") String rating, @Query("api_key") String API_KEY);
+
+    @GET("gifs")
+    Call<GIFModelMain> getGifsByID(@Query("ids") String tag, @Query("api_key") String API_KEY);
+
 
     //
     @GET("stickers/search")
