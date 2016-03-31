5c2a6ebefd24158b42364be2b2ea4aa74a5adc21
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2016-03-31 22:57:35 +0530

    Changed Model for FavouriteGifs.

--- app/src/main/java/com/nerdcutlet/gift/models/FavouriteGif.java
+++ app/src/main/java/com/nerdcutlet/gift/models/FavouriteGif.java
@@ -2,20 +2,79 @@ package com.nerdcutlet.gift.models;
 
 import com.orm.SugarRecord;
 
+import java.util.Date;
+
 /**
  * Created by Aldrich on 29-03-2016.
  */
 public class FavouriteGif extends SugarRecord {
-     String mid;
+    String mGifID;
+
+    String mDate;
+    /*
+    *FORMAT : 'YYYY-mm-dd hh:mm:ss'
+    *         24 hour format
+    *         '2015-08-11 13:13:00' (time for 1:13pm exactly).
+    */
+
+    String mTag;
+    /*
+    searchData
+    trendingGifs
+    receivedGifs
+    TODO: Add random
+     */
+
+
+    int mTypeOfData;
+    /*
+    0 - Gifs. [received gifs/stickers]
+    2 - Trending Gifs.
+    1 - Stickers [in app search ONLY]
+     */
+
+    public int getmTypeOfData() {
+        return mTypeOfData;
+    }
+
+    public void setmTypeOfData(int mTypeOfData) {
+        this.mTypeOfData = mTypeOfData;
+    }
+
+    public FavouriteGif(String mGifID, int mTypeOfData, String mTag, String mDate) {
+        this.mGifID = mGifID;
+        this.mTypeOfData = mTypeOfData;
+        this.mTag = mTag;
+
+        this.mDate = mDate;
+    }
+
+    public String getmGifID() {
+        return mGifID;
+    }
+
+    public void setmGifID(String mGifID) {
+        this.mGifID = mGifID;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
+    }
+
+    public String getmTag() {
+        return mTag;
+    }
+
+    public void setmTag(String mTag) {
+        this.mTag = mTag;
+    }
+
 
     public FavouriteGif() {
     }
 
-    public FavouriteGif(String id) {
-        this.mid = id;
-    }
-
-    public String getGifId(){
-        return mid;
-    }
 }
