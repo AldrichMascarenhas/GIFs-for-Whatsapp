9be81a631c8bdcbba8b9aedd347159678b914015
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2016-04-19 15:39:17 +0530

    OnClickListener Changes to pass Color

--- app/src/main/java/com/nerdcutlet/gift/views/OnItemClickListener.java
+++ app/src/main/java/com/nerdcutlet/gift/views/OnItemClickListener.java
@@ -1,11 +1,14 @@
 package com.nerdcutlet.gift.views;
 
+import android.graphics.drawable.Drawable;
 import android.view.View;
 
 /**
  * Created by Aldrich on 25-03-2016.
  */
 public interface OnItemClickListener {
-    public void onItemClick(View v, int position);
+    public void onItemClick(View v, int position, Drawable drawable);
+
+
 
 }
